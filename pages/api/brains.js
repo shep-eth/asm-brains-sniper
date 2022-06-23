81b00a96cfbe3062ef46fbd969cc2837639a235a
shep.eth <blhome520@gmail.com>
2022-06-24 11:32:12 +1200

    update genie api

--- pages/api/brains.js
+++ pages/api/brains.js
@@ -18,7 +18,7 @@ const client = new Redis(process.env.REDIS_URL);
 const fetchFromGenie = async (contractAddress, offset, limit) => {
   const res = await axios({
     method: "POST",
-    url: "https://v2.api.genie.xyz/assets",
+    url: "https://genie-production-api.herokuapp.com/assets",
     headers: {
       "User-Agent":
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36",
